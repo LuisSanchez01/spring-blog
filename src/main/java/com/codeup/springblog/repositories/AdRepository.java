5879f414cd4fe6d92aed6473fe58be9668362336
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-05-16 16:45:10 -0500

    add AdRepository

---
+++ src/main/java/com/codeup/springblog/repositories/AdRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.springblog.repositories;
+
+import com.codeup.springblog.models.Ad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdRepository extends JpaRepository<Ad, Long> {
+}
