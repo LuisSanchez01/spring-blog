200c80442a18035429b3815cb3e56a7d8dfda920
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-05-16 11:48:31 -0500

    add spring-blog

--- src/main/java/com/codeup/springblog/controllers/MathController.java
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -1,16 +1,38 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MathController {
-        @RequestMapping(path = "/add100To/{number}", method = RequestMethod.GET)
+    @RequestMapping(path = "/add100To/{number}", method = RequestMethod.GET)
     @ResponseBody
     public String addOneHundred(@PathVariable int number){
         return number + " + 100 = " + (number + 100);
     }
+
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public double add(@PathVariable double num1, @PathVariable double num2){
+        return num1 + num2;
+    }
+
+    @GetMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public double subtract(@PathVariable double num1, @PathVariable double num2){
+        return num2 - num1;
+    }
+
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public double multiply(@PathVariable double num1, @PathVariable double num2){
+        return num1 * num2;
+    }
+
+    @GetMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public double divide(@PathVariable double num1, @PathVariable double num2){
+        return num1/num2;
+    }
+
 }
---
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -0,0 +1,33 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/posts")
+public class PostController {
+
+    @GetMapping
+    @ResponseBody
+    public String allPosts(){
+        return "Here are all the posts!";
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public String onePost(@PathVariable long id){
+        return "Here is post number " + id;
+    }
+
+    @GetMapping("/create")
+    @ResponseBody
+    public String createPost(){
+        return "Here is the form to create a post!";
+    }
+
+    @PostMapping("/create")
+    @ResponseBody
+    public String submitPost(){
+        return "You just created a post!";
+    }
+}
