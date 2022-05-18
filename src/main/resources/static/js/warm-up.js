6beb1c9fecdae700e366b7bca1d6a8806be6b944
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-05-18 10:30:09 -0500

    add warm_up.js

---
+++ src/main/resources/static/js/warm-up.js
@@ -0,0 +1,16 @@
+// 1. Read through the curriculum lesson on JPA Relationships.
+
+// 2. Looking back to our curriculum on database design, what examples for the
+// following relationships can you identify?
+//     one-to-one
+//     one-to-many
+//     many-to-many
+
+// 3. Take a look at the pets database design posted in the Slack channel. What relationships do you see?
+// -Pet_Id is in 3 sections
+// -
+
+// 4. What annotations are needed in JPA to create a one-to-one, one-to-many, and many-to-many relationships?
+// -One-to-many: @OneToMany
+// -Many-to-many: @ManyToMany
+// -One-to-one: @OneToOne
